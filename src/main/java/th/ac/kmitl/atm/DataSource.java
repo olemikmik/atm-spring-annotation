a45bbbe9215f90e29cbd506dd229f91397945c08
unknown <mika.o25187@gmail.com>
2020-10-10 12:23:08 +0700

    61050819

---
+++ src/main/java/th/ac/kmitl/atm/DataSource.java
@@ -0,0 +1,10 @@
+package th.ac.kmitl.atm;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+public interface DataSource {
+    Map<Integer,Customer> readCustomers();
+}
